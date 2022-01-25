7d18a775fea7bf52111bda30f57a92d217fe32c9
aamandahiya <aman1815605@gmail.com>
2022-01-25 10:14:20 +0530

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+
+import {FullstackdevelopComponent} from './fullstackdevelop/fullstackdevelop.component'
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule],
+ 
+  declarations: [ AppComponent, HelloComponent , FullstackdevelopComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
